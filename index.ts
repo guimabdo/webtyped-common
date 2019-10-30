ecbc054b811f5dfecda3d89d7d4da4d6a9254e14
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2019-10-30 17:16:50 -0300

    exposing fetch invoker

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 ﻿export * from "./webTypedEventEmitter";
+export * from "./webTypedFetchInvoker";
 export * from "./webTypedCallInfo";
 export * from "./webTypedFunction";
 export * from "./webTypedInvoker";
